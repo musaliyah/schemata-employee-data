d3a06403f0432249f86eac2c17e552cf5240b738
musaliyah <asmusaliar@gmail.com>
2022-08-24 13:54:32 -0700

    more factors added to tables

--- schema.sql
+++ schema.sql
@@ -5,16 +5,22 @@ USE employee_DB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+    department_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title, 
-    department_id,
-    salary
+    title VARCHAR(50) NOT NULL, 
+    department_id INT,
+    salary INT,
 )
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    job_title VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary INT,
+    manager VARCHAR(30) NOT NULL
 )
 
