ce27987ae34eae8268729858014b7db186f28671
musaliyah <asmusaliar@gmail.com>
2022-08-24 13:35:28 -0700

    following Anthony's demo

---
+++ server.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+const express = require('express');
+const mysql = require('mysql2');
+
+//View all departments 
+//SELECT * FROM department;
+
+//View all employees
+//SELECT * FROM employees;
+
+//Create a new departments
+
+//prompt the user for the "name" of the department 
+
+    //THEN run the query 
+    //INSERT INTO department (name)
+    //VALUES ("Sales");
+
+// Create a new role 
+
+// Get the existing departments from the "department" table
+
+    //Then //prompt user for the title "salary" and "department" for the role
+        
+        //THEN  Run the query
+
+    // INSERT INTO role (title, salary, department_id)
+    // VALUES ("Engineer", 120000, 1)
