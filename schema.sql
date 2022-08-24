904b0d8cd609129348cd51ba0507de71d80ae773
musaliyah <asmusaliar@gmail.com>
2022-08-24 15:10:45 -0700

    data added to seed file

--- schema.sql
+++ schema.sql
@@ -8,13 +8,13 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id)
-        REFERENCES (department (id))
+        REFERENCES department (id)
         ON DELETE SET NULL
 );
 
@@ -23,11 +23,12 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT
-    manager_id INT NULL
+    role_id INT,
+    manager_id INT NULL,
+
     FOREIGN KEY (role_id)
         REFERENCES role(id)
-        ON DELETE SET NULL
+        ON DELETE SET NULL,
 
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
--- seed.sql
+++ seed.sql
@@ -1,2 +1,33 @@
 INSERT INTO department (name)
+VALUES 
+("Sales"), 
+("Engineering"), 
+("Finance"), 
+("Legal");
+
+SELECT * FROM DEPARTMENT; 
+
+INSERT INTO roles (title, salary, department_id)
 VALUES
+    ("Sales Lead", 100000, 1),
+    ("Salesperson", 80000, 1),
+    ("Lead Engineer", 150000, 2),
+    ("Software Engineer", 120000,2),
+    ("Account Manager", 160000, 3),
+    ("Accountatnt", 125000, 3),
+    ("Legal Team Lead", 250000, 4),
+    ("Lawyer", 190000, 4);
+
+SELECT * FROM roles;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+("Walter", "White", 2, NULL),
+("Gus", "Fring", 3, NULL),
+("Saul", "Goodman", 4, NULL),
+("Jesse", "Pinkman", 2, 1),
+("Skyler", "White", 1, 4),
+("Mike", "Ehrmantraut", 3, 2);
+
+SELECT * FROM employee;
+
