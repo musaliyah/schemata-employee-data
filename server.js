90fd0ddb6efad57319a90684a3af6ca95f2e195b
musaliyah <asmusaliar@gmail.com>
2022-08-24 15:21:02 -0700

    adding to the server.js file

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
+const consoleTbl = require('console.table');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 //View all departments 
 //SELECT * FROM department;
