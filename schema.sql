f72abc46d8a7f019763133078a66f2372f853901
musaliyah <asmusaliar@gmail.com>
2022-08-24 14:06:23 -0700

    foreign keys added

--- schema.sql
+++ schema.sql
@@ -5,21 +5,32 @@ USE employee_DB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT
-)
+    FOREIGN KEY (department_id)
+        REFERENCES (department (id))
+        ON DELETE SET NULL
+);
+
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT
     manager_id INT NULL
+    FOREIGN KEY (role_id)
+        REFERENCES role(id)
+        ON DELETE SET NULL
+
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 )
 
